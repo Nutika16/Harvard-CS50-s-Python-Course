03e7ff01fe0faad086c35ab292ca2256b77573b0
Nutika <90458419+Nutika16@users.noreply.github.com>
2025-11-23 10:44:15 +0530

    While & For Loop example

---
+++ IntroductionToProgramming/Loops/cat.py
@@ -0,0 +1,12 @@
+# Print meaw any number of times you like using for loop
+# In this we are not at all using the variable called i so we can replace it with _ indicating that yes this is a var
+# But we don't care about what is the name of the value. 
+for _ in range(5):
+    print("meow")
+
+# Printing meow any number of time using while loop
+i=0
+while(i<3):
+    print("meow meow!!")
+    i += 1
+
