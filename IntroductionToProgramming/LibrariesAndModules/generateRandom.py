89a13bfa5e338aacf4d73881763f84a46619760b
Nutika <90458419+Nutika16@users.noreply.github.com>
2025-11-26 18:40:18 +0530

    Random
    In this, we learned how we can import the modules and use the perform different operations using the built-in functions

--- IntroductionToProgramming/LibrariesAndModules/generateRandom.py
+++ IntroductionToProgramming/LibrariesAndModules/generateRandom.py
@@ -7,3 +7,9 @@ print(coin)
 # random.randint(a, b) returns a random integer between a and b.
 num = random.randint(1,10)
 print(num)
+
+# random.shuffle(x) function is used for shuffling
+cards = ["jack" , "queen" , "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
